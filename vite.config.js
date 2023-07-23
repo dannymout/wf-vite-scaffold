ae2613b987c0103d420bc6479e4cb7fb3272d61d
dannymout <dannymout@users.noreply.github.com>
2023-07-23 11:47:10 -0400

    Upgrade to Vite v4, HTTPS Dev Server

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
+import mkcert from 'vite-plugin-mkcert'
 
 // vite.config.js
 export default defineConfig({
-  plugins: [eslintPlugin({ cache: false })],
-  server: {
-    host: 'localhost',
-    cors: '*',
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws',
-    },
-  },
-  build: {
-    minify: true,
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.js',
-      output: {
-        format: 'umd',
-        entryFileNames: 'main.js',
-        esModule: false,
-        compact: true,
-        globals: {
-          jquery: '$',
+    plugins: [mkcert(), eslintPlugin({ cache: false })],
+    server: {
+        host: 'localhost',
+        cors: {
+            origin: '*'
         },
-      },
-      external: ['jquery'],
+        port: '3000'
     },
-  },
+    build: {
+        minify: true,
+        manifest: false,
+        rollupOptions: {
+            input: './src/main.js',
+            output: {
+                format: 'umd',
+                entryFileNames: 'main.js',
+                esModule: false,
+                compact: true,
+                globals: {
+                    jquery: '$',
+                },
+            },
+            external: ['jquery'],
+        },
+    },
 })
